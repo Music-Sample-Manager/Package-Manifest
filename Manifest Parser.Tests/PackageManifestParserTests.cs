e87c8f3111c291e8c4e03961bf9389439e089c62
Daniel Neel <dneelyep@gmail.com>
2019-01-07 21:59:30 -0500

    Add some basic tests for the Package Manifest Parser.

--- Manifest Parser.Tests/PackageManifestParserTests.cs
+++ Manifest Parser.Tests/PackageManifestParserTests.cs
@@ -1,4 +1,6 @@
 using NUnit.Framework;
+using Manifest_Parser;
+using System;
 
 namespace Manifest_Parser.Tests
 {
@@ -10,9 +12,17 @@ namespace Manifest_Parser.Tests
         }
 
         [Test]
-        public void Test1()
+        public void ParseManifest_WithNullManifestPath_ThrowsArgumentNullException()
         {
-            Assert.Pass();
+            var sut = new PackageManifestParser();
+            Assert.Throws<ArgumentNullException>(() => sut.ParseManifest(null));
+        }
+
+        [Test]
+        public void ParseManifest_WithEmptyManifestPath_ThrowsException()
+        {
+            var sut = new PackageManifestParser();
+            Assert.Throws<ArgumentException>(() => sut.ParseManifest(string.Empty));
         }
     }
 }
--- Manifest Parser/PackageManifestParser.cs
+++ Manifest Parser/PackageManifestParser.cs
@@ -4,5 +4,17 @@ namespace Manifest_Parser
 {
     public class PackageManifestParser
     {
+        public void ParseManifest(string manifestFilePath)
+        {
+            if (manifestFilePath == null)
+            {
+                throw new ArgumentNullException(nameof(manifestFilePath));
+            }
+
+            if (manifestFilePath == string.Empty)
+            {
+                throw new ArgumentException(nameof(manifestFilePath));
+            }
+        }
     }
 }
