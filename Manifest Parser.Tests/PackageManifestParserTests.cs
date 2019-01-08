cd87cb19307b6add266a79fcc43c1cfe106f0107
Daniel Neel <dneelyep@gmail.com>
2019-01-07 20:20:41 -0500

    Add a stub unit test project for the package manifest parser.

---
+++ Manifest Parser.Tests/PackageManifestParserTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Manifest_Parser.Tests
+{
+    public class PackageManifestParserTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
